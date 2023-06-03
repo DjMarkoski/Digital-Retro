4bd720fa85cbb596a9adbc25ff6e1a7826bf4be3
DjMarkoski <marcobarahonaroa@gmail.com>
2023-06-02 22:23:12 -0400

    registro y login de usuario en desarrollo

---
+++ usuario/loginC.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="loginC.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Cliente</title>
+</head>
+
+<body>
+    <section>
+        <div class="form-box">
+            <div class="form-value">
+                <form acrion="">
+                    <h2>Login Digital Retro</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="email" required>
+                        <label for="">Email</label>
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" required>
+                        <label for="">Password</label>
+                    </div>
+                    <div class="forget">
+                        <label for=""><input type="checkbox" value="">Recordarme <a href="#">¿Olvido su contraseña? </a></label>
+                    </div>
+                    <button>Iniciar Sesión</button>
+                    <div class="register">
+                        <p>No tengo una cuenta <a href="">Registrarse</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
